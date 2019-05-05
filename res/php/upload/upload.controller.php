8436a927eafc05e70ac5b090f6d77f0325b73eca
doduandrei98 <dodu_andrei98@yahoo.com>
2019-05-05 19:50:49 +0300

    Upload
    modificari

--- res/php/upload/upload.controller.php
+++ res/php/upload/upload.controller.php
@@ -6,7 +6,8 @@ if ($_POST["submit"] == "Incarca")
 {
         
         $resultat_imagine=upload($_FILES["imagine_aleasa"],$_FILES["imagini_alese"],$_POST["Tag1"],$_POST["Tag2"],$_POST["Tag3"],$_POST["Tag4"],$_POST["Tag5"],$_POST["Titlu"],$_POST["Descriere"]);
-       
+        
+        
         if($resultat_imagine)
             header("location: ../../html/home.php");
             else
